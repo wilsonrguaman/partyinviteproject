22b50fc2065b65564bb8f3d7f06a06468698f2e1
wilsonrguaman <87439959+wilsonrguaman@users.noreply.github.com>
2021-09-17 14:51:34 -0500

    Add files via upload

---
+++ Models/GuestRespose.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace PartyInvites_Ass2_WilsonGuaman.Models
+{
+    public class GuestRespose
+    {
+        [Required(ErrorMessage = "Please enter your name")]
+       public string Name { get; set; }
+        [Required(ErrorMessage ="Please enter your email address")]
+        [EmailAddress]
+       public string Email { get; set; }
+        [Required(ErrorMessage ="Please enter your phone number")]
+       public string Phone { get; set; }
+        [Required(ErrorMessage ="Please specify whether you'll attend")]
+       public bool? WillAttend { get; set; }
+
+    }
+}
---
+++ Models/Repository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+namespace PartyInvites_Ass2_WilsonGuaman.Models
+{
+    public class Repository
+    {
+        private static List<GuestRespose> responses = new List<GuestRespose>();
+        public static IEnumerable<GuestRespose> Resposes => responses;
+        public static void AddResponse(GuestRespose response)
+        {
+            responses.Add(response);
+        }
+        
+    }
+}
